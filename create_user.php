02a3e56f9fb2e56164839aae588252458a3b1080
Mrpasg <your-email@gmail.com>
2026-03-13 17:30:01 +0530

    user created page

---
+++ create_user.php
@@ -0,0 +1,37 @@
+<?php
+include "auth.php";
+include "config.php";
+
+if($_POST){
+
+$username=$_POST['username'];
+$password=md5($_POST['password']);
+$role=$_POST['role'];
+
+$conn->query("
+INSERT INTO users (username,password,role)
+VALUES ('$username','$password','$role')
+");
+
+echo "User created successfully";
+
+}
+?>
+
+<form method="post">
+
+Username
+<input type="text" name="username">
+
+Password
+<input type="password" name="password">
+
+Role
+<select name="role">
+<option value="user">User</option>
+<option value="admin">Admin</option>
+</select>
+
+<button>Create User</button>
+
+</form>
